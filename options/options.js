ea61545e3a024e5b57fdc474d6088bc54c87180e
Crowdbotics <team@crowdbotics.com>
2023-07-08 23:50:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hisolar-42011",
+  url: "https://hisolar-42011.botics.co",
+  api: "https://hisolar-42011.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
